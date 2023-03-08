2a2ab9a26890a8bc7f9bc7bc841c63d18acc9a68
Dzigisha <igorilic55@gmail.com>
2023-03-08 01:18:15 +0100

    Dictionary sa nazivom i cenom

--- scrapper_pavlaka.py
+++ scrapper_pavlaka.py
@@ -1,27 +1,16 @@
-# from bs4 import BeautifulSoup
-# import requests
-
-# url = "https://cenoteka.rs/proizvodi/mlecni-proizvodi/pavlaka"
-# result = requests.get(url)
-# doc = BeautifulSoup(result.text,"html.parser")
-# rez = doc.find_all("Kisela pavlaka IMLEK Moja kravica 20%mm 400g")
-# print(rez)
 import requests
 from bs4 import BeautifulSoup
 
-# The URL of the page we want to scrape
 url = "https://cenoteka.rs/proizvodi/mlecni-proizvodi/pavlaka"
-
-# Send a GET request to the URL
 response = requests.get(url)
-
-# Parse the HTML content of the page using Beautiful Soup
 doc = BeautifulSoup(response.content, 'html.parser')
-
-# Find all the products listed on the page
-# products = soup.find_all('section', class_='row section list-articles-content box-shadow bg-white')
-cene = doc.find_all(text='Kisela pavlaka IMLEK Moja kravica 20%mm 400g')
+cene = doc.find_all(text='Kisela pavlaka IMLEK Moja kravica 20%mm 180g')
 parent = cene[0].parent.parent.parent
-children = parent.find('div','price lowest akcija star-top p-0')
-
-print(children.text)
+parent2 = cene[0].parent.parent.parent
+children = parent.find_all('div','article-price flex-full-height-center border-gray-right text-center')
+dicts = {}
+for x in children:
+    slika = x.find('img',alt=True)
+    cena = x.find('div','price').text.strip()
+    dicts[slika['alt']]=cena
+min_key = [k for k, v in dicts.items() if v == min(dicts.values())]
